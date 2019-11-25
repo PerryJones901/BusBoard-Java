18a44e1c0c9bc70cfcb5d63de5d67f2fbc0c5882
Lucas Nogueira <lucas.nogueira@softwire.com>
2019-11-25 13:48:09 +0000

    Before lunch

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -5,15 +5,19 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 
 public class Main {
     public static void main(String args[]) {
 
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        String request = client.target("https://api.tfl.gov.uk/StopPoint/490008660N")
+        StopInfo myStop = new StopInfo();
+        List<StopInfo> request = client.target("https://api.tfl.gov.uk/StopPoint/490008660N")
                 .request(MediaType.APPLICATION_JSON)
-                .get(String.class);
+                .get(List<StopInfo.class>);
 
         System.out.println(request);
     }
+
 }	
---
+++ src/main/java/training/busboard/StopInfo.java
@@ -0,0 +1,7 @@
+package training.busboard;
+
+public class StopInfo {
+    String lineId;
+    String destinationName;
+    String expectedArrival;
+}
